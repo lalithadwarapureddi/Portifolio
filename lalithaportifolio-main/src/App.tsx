d96356b7f35a022290a5057339edffb4d25c29ca
lalithadwarapureddi <133760887+lalithadwarapureddi@users.noreply.github.com>
2025-09-13 23:51:32 +0530

    first commit

--- lalithaportifolio-main/src/App.tsx
+++ lalithaportifolio-main/src/App.tsx
@@ -12,14 +12,14 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <BrowserRouter basename="/Portifolio">
+  <Routes>
+    <Route path="/" element={<Index />} />
+    {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+</BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
--- lalithaportifolio-main/vite.config.ts
+++ lalithaportifolio-main/vite.config.ts
@@ -5,11 +5,13 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/Portifolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
